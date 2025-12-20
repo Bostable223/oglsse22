e3bd6469a0526eeee26eabc32fb919d0122947b0
Msp <bostable@gmail.com>
2025-12-20 18:25:20 +0100

    Controller fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Package;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
