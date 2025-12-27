cba3d3a72e3052882a60553067d9759a4a9d0bbf
Msp <bostable@gmail.com>
2025-12-27 16:18:11 +0100

    Maintainance

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::call(function () {
+    // Delete pending listings older than 14 days
+    Listing::where('status', 'pending')
+           ->where('created_at', '<', now()->subDays(14))
+           ->forceDelete();
+           
+    // Delete read notifications older than 60 days
+    (new NotificationService)->cleanupOldNotifications();
+})->daily();
